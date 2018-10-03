e401e4a577a53cc460ab08a7dc5464b9bd80fe3b
Siddharth Jain <siddharth@cuelearn.com>
2018-10-03 21:08:04 +0530

    socket

--- skt.py
+++ skt.py
@@ -6,11 +6,6 @@ import time
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-values = {
-    'slider1': 25,
-    'slider2': 0,
-}
-
 try:
     ser = serial.Serial("/dev/ttyACM0", 9600, timeout=3)  # change ACM number as found from ls /dev/tty/ACM*
 except:
@@ -22,26 +17,30 @@ ser.baudrate = 9600
 def index():
     return render_template('index.html', **values)
 
+
 @socketio.on('connect')
 def test_connect():
-    while True:
-        data = ser.readline()
-        data2 = data.decode("UTF-8")
-        position_case = data2.find('bat')
-        cases = data2[position_case + 5:position_case + 6]
-        position_Ah1 = data2.find('Ah1')
-        position_Ah2 = data2.find('Ah2')
-        if position_Ah1 < 0:
-            Ah1 = 0.0
-            Ah2 = 0.0
-        else:
-            Ah1 = float(data2[position_Ah1 + 5:position_Ah1 + 11])
-            Ah2 = float(data2[position_Ah2 + 5:position_Ah2 + 11])
-        print(Ah1, Ah2, cases)
-        emit('after connect',  {'data': {'per1': 20, 'per2': 40, 'case': 1}})
+    emit('after connect',  {'data': {'per1': 20, 'per2': 40, 'case': 3}})
 
+
+@socketio.on('get new value')
+def get_new_value(msg):
+    time.sleep(1)
+    data = ser.readline()
+    data2 = data.decode("UTF-8")
+    position_case = data2.find('bat')
+
+    cases = int(data2[position_case + 5:position_case + 6])
+    position_Ah1 = data2.find('Ah1')
+    position_Ah2 = data2.find('Ah2')
+    if position_Ah1 < 0:
+        Ah1 = 0.0
+        Ah2 = 0.0
+    else:
+        Ah1 = float(data2[position_Ah1 + 5:position_Ah1 + 11])
+        Ah2 = float(data2[position_Ah2 + 5:position_Ah2 + 11])
+
+    emit('after connect', {'data': {'per1': 20, 'per2': 40, 'case': 3}}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
-
-
